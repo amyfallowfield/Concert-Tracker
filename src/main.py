35853569bb32c8050a48fff07762029c76d48643
amyfallowfield <amyfallowfield123@gmail.com>
2026-01-24 13:06:32 +0000

    Implemented venue database + gitignore

--- src/main.py
+++ src/main.py
@@ -2,11 +2,13 @@ import streamlit as st
 import pandas as pd
 import os
 from pathlib import Path
+import TicketmasteAPI
 
 class GUI:
 
     def __init__(self):
         os.makedirs("data", exist_ok=True)
+        self.api = TicketmasteAPI.LocationData()
 
         try:
             self.file_path = Path("data/concerts-attended.csv")
@@ -16,13 +18,19 @@ class GUI:
 
         st.set_page_config(page_title="Concert Tracker", layout="wide")
 
+        self.generate_concert_data_page()
+
     def generate_concert_data_page(self):
-        self.generate_summary()
+        if len(self.df) > 0:
+            self.generate_summary()
+        else:
+            st.write("Add your first event here")
+
         self.generate_add_event()
         self.generate_view_events()
     
     def generate_summary(self):
-        cost = self.df["Cost"].sum()
+        cost = round(self.df["Cost"].sum(), 2)
         shows = len(self.df)
         fav_artist_name = self.df["Artist"].mode()[0]
         fav_artist_count = self.df[self.df["Artist"] == fav_artist_name]["Artist"].count()
@@ -30,7 +38,6 @@ class GUI:
         fav_venue_count = self.df[self.df["Venue"] == fav_venue_name]["Venue"].count()
 
         col1, col2, col3, col4 = st.columns(4)
-
         with col1:
             st.metric("Total Spent", f"£{cost}")
         with col2:
@@ -41,33 +48,42 @@ class GUI:
             st.metric("Favourite Venue", f"{fav_venue_name} (x{fav_venue_count})")
 
     def generate_view_events(self):
-        st.write(self.df)
+        st.dataframe(self.df)
 
     def generate_add_event(self):
         
-        col1, col2, col3, col4 = st.columns(4)
+        col1, col2, col3 = st.columns(3)
         with col1:
             artist = st.text_input("Enter artist name")
+            date = st.date_input("Enter date", value="today", format="DD/MM/YYYY")
         with col2:
             tour = st.text_input("Enter tour name")
+            cost = st.number_input("Enter ticket cost")
         with col3:
             venue = st.text_input("Enter venue name")
-        with col4:
-            cost = st.text_input("Enter ticket cost")
+            supports = st.text_area("Enter support(s) [Separate each artist with a comma]")
+            
+        supports_list = supports.split(",")
+        for i in supports_list:
+            i.strip()
 
-        st.button("Create new event", on_click=self.add_event, args=[artist, tour, venue, cost])
+        st.button("Create new event", on_click=self.add_event, args=[artist, tour, venue, cost, date, supports_list])
 
-    def add_event(self, artist, tour, venue, cost):
-        new_entry = [artist, tour, venue, cost]
-        if not all(new_entry):
+    def add_event(self, artist, tour, venue, cost, date, supports):
+
+        new_entry = [artist, tour, venue, date, cost, supports]
+        if not all(new_entry[:-3]):
             st.write("Please fill out all text boxes")
             return
-        
+
         new_row = {
             "Artist": artist,
             "Tour": tour,
             "Venue": venue,
-            "Cost": cost
+            "Location": self.api.get_location_data(venue),
+            "Cost": cost,
+            "Date": date,
+            "Supports": supports,
         }
 
         self.df = pd.concat(
@@ -76,7 +92,5 @@ class GUI:
         )
         self.df.to_csv(self.file_path, index=False)
 
-
 if __name__ == "__main__":
     gui = GUI()
-    gui.generate_concert_data_page()
